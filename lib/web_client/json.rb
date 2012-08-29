82fdec403c76604fa567b83f85aa638c418683d4
gabriel <gnaiman@keepcon.com>
2012-08-29 18:45:53 -0300

    JSON contains base client

--- lib/web_client/json.rb
+++ lib/web_client/json.rb
@@ -1,10 +1,14 @@
 module WebClient
-  class JSON < Base
+  class JSON
+
+    def initialize(*args)
+      @http_client = Base.new(*args)
+    end
 
     WebClient::HTTP_METHODS.each do |http_method|
       method_name = http_method.to_s.demodulize.downcase
       define_method method_name do |*args, &block|
-        response = Base.instance_method(method_name).bind(self).call(*args, &block)
+        response = @http_client.send(method_name, *args, &block)
         response ? ::JSON.parse(response.body || '{}') : nil
       end
     end
